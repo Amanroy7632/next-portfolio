5252dfdbeaabf0ebde0f05f57d35a91e3cab0c86
Aman Roy <yadavaman7632@gmail.com>
2025-08-26 21:18:26 +0530

    First commit

---
+++ src/lib/portfolioConfig.ts
@@ -0,0 +1,63 @@
+export const portfolioConfig = {
+  // Basic Information
+  name: "Aman Kumar Yadav",
+  title: "Software Engineer",
+  description: "I am a Passionate Software Developer",
+  location: "Dwarka sector-7, Delhi, India",
+
+  // Contact Information
+  email: "iamandaa.dev@gmail.com", 
+
+  // Social Media Links
+  socialLinks: {
+    facebook: "https://www.facebook.com/shahina.khatun.1044",
+    twitter: "https://twitter.com/Taquiimam14",
+    linkedin: "https://www.linkedin.com/in/taqui-imam",
+    external: "https://tinyurl.com/MdTaquiImam",
+    github: "https://github.com/taqui-786", 
+  },
+
+  // SEO Information
+  seo: {
+    ogImage: "https://taqui-imam.vercel.app/og-image.png",
+    url: "https://taqui-imam.vercel.app",
+    twitterHandle: "@amanroy7632",
+    keywords: [
+      "aman",
+      "kumar",
+      "yadav",
+      "AMAN",
+      "KUMAR",
+      "YADAV",
+      "Aman Kumar Yadav",
+      "aman kumar yadav",
+      "amankumaryadav",
+      "portfolio",
+      "web developer",
+      "web",
+      "web dev",
+      "developer",
+      "PROGRAMMER ",
+      "programmer ",
+      "website",
+      "@amanroy7632",
+      "aman developer",
+    ],
+    authors: [
+      {
+        name: "Aman Kumar Yadav",
+        url: "https://github.com/amanroy7632",
+      },
+    ],
+  },
+
+
+
+
+
+
+
+
+
+
+};
